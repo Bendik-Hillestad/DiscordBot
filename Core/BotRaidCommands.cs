267e0416e2d2ce3b9dcab310406336899c1e62d4
Bendik Hillestad <bendik.hillestad@gmail.com>
2018-03-18 13:33:19 +0100

    Fixed a null-pointer dereference.

--- Core/BotRaidCommands.cs
+++ Core/BotRaidCommands.cs
@@ -1179,7 +1179,7 @@ namespace DiscordBot.Core
             bool bu = false;
 
             //Check if one of the roles is BACKUP
-            if (roles.Contains("BACKUP"))
+            if (roles?.Contains("BACKUP") ?? false)
             {
                 //Set flag
                 bu = true;
@@ -1189,7 +1189,7 @@ namespace DiscordBot.Core
             }
 
             //Check that we got at least one
-            if (roles != null && roles.Count > 0)
+            if ((roles?.Count ?? 0) > 0)
             {
                 //Get the username/nickname
                 string name = (msg.Author as SocketGuildUser)?.Nickname ?? msg.Author.Username;
