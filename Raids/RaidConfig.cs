afa4fe9768ba7675191222fd02ebf6a63ec5380c
Bendik-Hillestad <bendik.hillestad@gmail.com>
2018-03-25 13:59:21 +0200

    Begun work on generating code for the solvers.

--- Raids/RaidConfig.cs
+++ Raids/RaidConfig.cs
@@ -84,9 +84,90 @@ namespace DiscordBot.Raids
             }
         }
 
-        public void GenerateSolver()
+        public void GenerateSolverDLL()
         {
             //TODO
+        }
+
+        public void GenerateImplementationsHeader()
+        {
+            //Grab the composition names
+            var compNames = this.Compositions.Select((c) => c.Name);
+
+            //Prepare the text
+            var str = $"#ifndef BILLY_HERRINGTON_IMPLEMENTATIONS_H\n" +
+                      $"#define BILLY_HERRINGTON_IMPLEMENTATIONS_H\n" +
+                      $"#pragma once\n" +
+                      $"\n" +
+                      $"namespace billy_herrington\n" +
+                      $"{{\n" +
+                      $"    enum class Implementation : unsigned int\n" +
+                      $"    {{\n" +
+                      $"        {string.Join(",\n        ", compNames)},\n" +
+                      $"        COUNT\n" +
+                      $"    }};\n" +
+                      $"}};\n" +
+                      $"\n" +
+                      $"#endif\n";
+
+            //Write to file
+            File.WriteAllText("implementations.h", str);
+        }
+
+        public void GenerateSolverCPP(/*CompDescription compDescription*/)
+        {
+            var str = "#include \"typelist.h\"\n" +
+                      "#include \"solver.h\"\n" +
+                      "#include \"registry.h\"\n" +
+                      "#include \"implementations.h\"\n" +
+                      "\n" +
+                      "#include <cstring>\n" +
+                      "\n" +
+                      "using namespace billy_herrington;\n" +
+                      "\n" +
+                      "/* Configuration */\n" +
+                      "\n" +
+                      "struct MES;\n" +
+                      "struct HEAL;\n" +
+                      "struct DPS;\n" +
+                      "struct SLAVE;\n" +
+                      "struct KITER;\n" +
+                      "\n" +
+                      "using roles       = tl::typelist_t<MES, HEAL, DPS, SLAVE, KITER>;\n" +
+                      "using composition = tl::typelist_t\n" +
+                      "<\n" +
+                      "    MES, HEAL, DPS, DPS, SLAVE,\n" +
+                      "    MES, HEAL, DPS, DPS,\n" +
+                      "    KITER\n" +
+                      ">;\n" +
+                      "\n" +
+                      "using solver_config = comp_solver::solver_config<roles, composition>;\n" +
+                      "using solver        = comp_solver::solver       <solver_config>;\n" +
+                      "\n" +
+                      "/* Specialized implementation */\n" +
+                      "\n" +
+                      "void solve_dhuum\n" +
+                      "(\n" +
+                      "    solver_config::roster_t::const_pointer roster, int length,\n" +
+                      "    solver_config::comp_t::pointer output\n" +
+                      ") noexcept\n" +
+                      "{\n" +
+                      "    //Create the solver\n" +
+                      "    solver s{ roster, length };\n" +
+                      "\n" +
+                      "    //Solve it!\n" +
+                      "    s.solve();\n" +
+                      "\n" +
+                      "    //Write solution to the output\n" +
+                      "    std::memcpy(output, s.get_solution().data(), sizeof(solver_config::comp_t));\n" +
+                      "}\n" +
+                      "\n" +
+                      "/* Registering */\n" +
+                      "\n" +
+                      "static bool r = registry::register_function(Implementation::DHUUM, reinterpret_cast<registry::solver_func>(&solve_dhuum));\n";
+
+            //Write to file
+            File.WriteAllText("solver_dhuum.cpp", str);
         }
     }
 }
