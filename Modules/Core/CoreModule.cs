d49ae9c4a219b318a6a20d0165c591d73d00b254
Bendik Hillestad <bendik.hillestad@gmail.com>
2018-05-17 13:02:55 +0200

    Added a core module.

---
+++ Modules/Core/CoreModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Discord;
+
+using DiscordBot.Commands;
+
+namespace DiscordBot.Modules.Core
+{
+    public partial class CoreModule : CommandModule<CoreModule>
+    {
+        public override string ModuleName => "Misc";
+
+        public override void OnInit()
+        { }
+
+        [Command("time")]
+        public void time(Context ctx)
+        {
+            //Get UTC+0
+            var utcNow = DateTimeOffset.UtcNow;
+
+            //FIXME: Check for DST
+            bool dst = true;//DateTime.Now.IsDaylightSavingTime();
+
+            //Reliably get the actual time
+            var now = utcNow.AddHours(dst ? 2 : 1);
+
+            //Setup the embed builder
+            var builder = new EmbedBuilder().WithColor(Color.Blue)
+                                            .WithDescription("Current time");
+
+            //Add times
+            var embed = builder.AddInlineField( "Servertime:",       $"{utcNow.Hour,2}:{utcNow.Minute,2}")
+                               .AddInlineField($"CE{(dst?"S":"")}T", $"{now.Hour,2}:{now.Minute,2}")
+                               .WithFooter    ( "Local time:").WithTimestamp(utcNow);
+
+            //Send the message
+            ctx.message.Channel.SendMessageAsync("", false, embed).GetAwaiter().GetResult();
+        }
+
+        public override string HelpMessage(Context ctx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
