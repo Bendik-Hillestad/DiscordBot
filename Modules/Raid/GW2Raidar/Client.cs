a5826005cb52bd18f47056e4d2f175823a7ff250
Bendik-Hillestad <bendik.hillestad@gmail.com>
2018-05-28 18:35:49 +0200

    Added function for listing encounters.

--- Modules/Raid/GW2Raidar/Client.cs
+++ Modules/Raid/GW2Raidar/Client.cs
@@ -1,15 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
 using DiscordBot.Core;
 using DiscordBot.Utils;
 
+using Newtonsoft.Json;
+
 namespace DiscordBot.Modules.Raid.GW2Raidar
 {
     public static class Client
     {
-        private static readonly string UPLOAD_URI = @"https://www.gw2raidar.com/api/v2/encounters/new";
+        private static readonly string UPLOAD_URI          = @"https://www.gw2raidar.com/api/v2/encounters/new";
+        private static readonly string LIST_ENCOUNTERS_URI = @"https://www.gw2raidar.com/api/v2/encounters?offset={0}&since={1}";
 
         public static bool UploadLog(string path)
         {
@@ -19,11 +25,8 @@ namespace DiscordBot.Modules.Raid.GW2Raidar
                 //Check if the file exists
                 Debug.Assert(File.Exists(path), $"{path} does not exist!");
 
-                //Check that we have a token
-                Debug.Assert(BotConfig.Config.Contains("gw2raidar_token"), "A GW2Raidar token is required!");
-
                 //Get the token
-                var token = (string)BotConfig.Config["gw2raidar_token"];
+                var token = GetToken();
 
                 //Read the file
                 var file = new StreamContent(File.OpenRead(path));
@@ -46,6 +49,73 @@ namespace DiscordBot.Modules.Raid.GW2Raidar
                     ret.EnsureSuccessStatusCode();
                 }
             });
+        }
+
+        public static List<EncounterResult> ListEncounters(int limit, DateTimeOffset since)
+        {
+            //Catch any errors
+            return Debug.Try(() =>
+            {
+                //Get the token
+                var token = GetToken();
+
+                //Get the number of seconds since the UNIX epoch
+                var timestamp = since.ToUnixTimeSeconds();
+
+                //Format our initial request
+                var next = string.Format(LIST_ENCOUNTERS_URI, 0, timestamp);
+
+                //Grab as many encounters as we can until we get enough
+                var results = new List<EncounterResult>();
+                do
+                {
+                    //Send our request
+                    var resp = ListEncounters(token, next);
+
+                    //Grab the results
+                    results.AddRange(resp.results);
+
+                    //Check if we got enough results
+                    if (results.Count >= limit) break;
+
+                    //Get the next uri
+                    next = resp.next;
+                } while (next != null);
+
+                //Return just enough elements
+                return results.Take(limit).ToList();
+            }, new List<EncounterResult>());
+        }
+
+        private static ListResponse ListEncounters(string token, string next)
+        {
+            //Get a HttpClient
+            using (var http = new HttpClient())
+            {
+                //Put our token in the header
+                http.DefaultRequestHeaders.Add("Authorization", $"Token {token}");
+
+                //Send the request
+                var ret = http.GetAsync(next).GetAwaiter().GetResult();
+
+                //Check if it was successful
+                ret.EnsureSuccessStatusCode();
+
+                //Read the json text
+                var jsonText = ret.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                //Parse JSON and return ListResponse object
+                return JsonConvert.DeserializeObject<ListResponse>(jsonText);
+            }
+        }
+
+        private static string GetToken()
+        {
+            //Check that we have a token
+            Debug.Assert(BotConfig.Config.Contains("gw2raidar_token"), "A GW2Raidar token is required!");
+
+            //Get the token
+            return (string)BotConfig.Config["gw2raidar_token"];
         }
     }
 }
---
+++ Modules/Raid/GW2Raidar/ListResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Modules.Raid.GW2Raidar
+{
+    public sealed class EncounterResult
+    {
+        public string url_id      { get; set; }
+        public long   started_at  { get; set; }
+        public int    area_id     { get; set; }
+        public int?   category_id { get; set; }
+        public string tags        { get; set; }
+        public bool   success     { get; set; }
+    }
+
+    public sealed class ListResponse
+    {
+        public int                   count   { get; set; }
+        public string                next    { get; set; }
+        public string                prev    { get; set; }
+        public List<EncounterResult> results { get; set; }
+    }
+}
