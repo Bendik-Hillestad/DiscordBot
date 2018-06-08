7b2c9cf618661eaaf3afb448215c04408ccfbfdd
Bendik-Hillestad <bendik.hillestad@gmail.com>
2018-06-08 15:32:45 +0200

    Added methods for interacting with undocumented API endpoints to circumvent issues with the official API. #11

--- Modules/Raid/GW2Raidar/Response/APIResponse.cs
+++ Modules/Raid/GW2Raidar/Response/APIResponse.cs
@@ -8,6 +8,23 @@ namespace DiscordBot.Modules.Raid.GW2Raidar.Response
         public string filename  { get; set; }
     }
 
+    public sealed class PollNotification
+    {
+        public string uploaded_by      { get; set; }
+        public string filename         { get; set; }
+        public string type             { get; set; }
+        public string encounter_url_id { get; set; }
+        public int    upload_id        { get; set; }
+        public int    encounter_id     { get; set; }
+    }
+
+    public sealed class PollResponse
+    {
+        public List<PollNotification> notifications { get; set; }
+        public string                 version       { get; set; }
+        public int?                   last_id       { get; set; }
+    }
+
     public sealed class EncounterResult
     {
         public string url_id      { get; set; }
